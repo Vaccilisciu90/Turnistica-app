a94cf90468a757ccc9fe2e630b20ef964728085c
Vaccilisciu90 <brunop90@hotmail.it>
2026-03-18 22:36:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='md-buona-spesa-italia-v2';
+const CACHE='md-buona-spesa-italia-fix-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
